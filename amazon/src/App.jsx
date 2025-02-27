69b8558858bdbfcea0d4646acf4982937826c0da
Kidus-Sofonias <sofoniaskidus@gmail.com>
2025-02-27 18:42:44 +0300

    category

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "/src/Components/Header/Header.jsx";
 import CarouselEffect from "/src/Components/Carousel/CarouselEffect.jsx";
+import Category from "/src/Components/Category/Category.jsx";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
       <CarouselEffect />
+      <Category />
     </div>
   );
 }
